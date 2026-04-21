dd7794e893a5f62faec76ecee83e61b120e3cc15
itz-biswajit021 <142914866+itz-biswajit021@users.noreply.github.com>
2026-04-21 19:20:47 +0530

    Initial portfolio commit

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -104,7 +104,7 @@ const Portfolio = () => {
   return (
     <div className={`portfolio ${darkMode ? "dark" : ""}`} id="portfolio">
       {/* HEADINGS */}
-      <span style={{ color: darkMode ? "white" : "" , fontSize: "2.2rem", fontWeight: "600" }}>
+      <span style={{ color: darkMode ? "white" : "" , fontSize: "2.2rem", fontWeight: "700" }}>
         Recent Projects
       </span>
       <span style={{ color: "#FCBF49" , fontSize: "1.8rem", fontWeight: "600" }}>
